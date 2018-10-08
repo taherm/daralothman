30440c3bc0984f9ecaba6966cfffca27e4746c25
Taher Mukadam <T.Mukadam@diyarme.com>
2018-10-08 19:43:56 +0300

    added image

--- resources/views/abdullah-othman.blade.php
+++ resources/views/abdullah-othman.blade.php
@@ -12,34 +12,8 @@
                 المحسن الكبير عبد الله عبد اللطيف العثمان
             </h2>
 
+ <img class="img-thumbnail" src="{{asset('/uploads/alothman.jpg')}}">
 
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                   
-                </ol>
-                <div class="carousel-inner"  style="width: auto;
-  max-height: 225px;">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="{{asset('uploads/home.jpg')}}" alt="First slide" style="width: 100%;
-    height: auto;">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="{{asset('uploads/home.jpg')}}" alt="Second slide" style="width: 100%;
-    height: auto;">
-                    </div>
-                    
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
 
             <p>
                 من خيرة رجالات العمل الخيري الكويتي وسليل أسرة كويتية عريقة من بيوت العلم والدين والخير ألا وهي أسرة
