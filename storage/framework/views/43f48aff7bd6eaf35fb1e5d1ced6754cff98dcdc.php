8eab36cddb85abf9bed848ac492afb51cba44cb8
Taher Mukadam <T.Mukadam@diyarme.com>
2018-12-25 18:57:20 +0300

    added books

--- storage/framework/views/43f48aff7bd6eaf35fb1e5d1ced6754cff98dcdc.php
+++ storage/framework/views/43f48aff7bd6eaf35fb1e5d1ced6754cff98dcdc.php
@@ -1,66 +1,136 @@
 <?php $__env->startSection('content'); ?>
 
 <br><br>
-			
-			<div class="container">
-            <div class="row">
 
-<div class="col-lg-6 text-center">
-<a href="/uploads/AL-OTHMANBOOK2.pdf"><img src="<?php echo e(asset('/uploads/version1.jpg')); ?>" width="300" height="500"></a>
+<div class="container">
+  <div class="row">
 
-</div>
+    <div class="col-lg-6 text-center">
+      <img src="<?php echo e(asset('/uploads/book1.jpg')); ?>" width="300" height="500">
 
-<div class="col-lg-6 text-center">
-<a href="/uploads/AL-OTHMANBOOK2.pdf"><img src="<?php echo e(asset('/uploads/version2.jpeg')); ?>" width="300" height="500"></a>
+    </div>
 
-</div>
-</div>
+    <div class="col-lg-6 text-center">
+      <img src="<?php echo e(asset('/uploads/book7.jpg')); ?>" width="300" height="500">
+
+    </div>
+  </div>
 
   <div class="row">
-  <div class="col-lg-6 text-center">
-  <p>
-  مدرسة العثمان
-</p>
-كتاب يوثق حقبة تاريخية مهمة في دولة الكويت، وهي حقبة المدارس الأهلية، وهي المدرسة التي أسسها المرحوم عبدالله عبداللطيف العثمان مع أخوته الملا عثمان والملا محمد والملا عبدالعزيز بين عامي 1931 و 1946. 
-</div>
-<div class="col-lg-6 text-center">
-<p>
-عثمان هذا الزمان
-</p>
-كتيب مبسط يحكي سيرة المرحوم عبد الله عبد اللطيف العثمان، وتم اعداده بالتعاون مع مؤسسة التواصل في اليمن.
-</div>
-</div>
-<br><br><br>
-<div class="row">
-<div class="col-lg-6 text-center">
-<img src="<?php echo e(asset('/uploads/version3.jpeg')); ?>" width="300" height="500">
+    <div class="col-lg-6 text-center">
+      <p>
+        مدرسة العثمان
+      </p>
+      تلقى كتاب مدرسة العثمان: رؤية تاريخية تربوية في أوراق مدرسة أهلية كويتية، اعداد: م. عدنان عبد الله العثمان صدىً
+      طيبًا ونجاحًا باهرًا على مستوى الأفراد والجهات الإعلامية في دولة الكويت.
+      ومن أهم النقاط التي ساهمت في إنجاح الكتاب تجاوزه للقالب العائلي المحدود فيما يخص تدوين السيرة العائلية والانطلاق
+      إلى أفق التاريخ الكويتي في ميدان التربية والتعليم.
+      ويستهل الكتاب بإهداء إلى كافة الطلبة في مدرسة العثمان والذين تتجاوز أعدادهم الثلاثمائة وخمسون طالبًا مع ذكر
+      أسمائهم, وكذلك أسماء المعلمين ممن شاركوا في التدريس في مدرسة العثمان.
+      كما وتم إهداء نسخة من كتاب "مدرسة العثمان" إلى فضيلة الإمام الأكبر شيخ الأزهر د. أحمد الطيب أثناء زيارة وضع حجر
+      الأساس لمركز المرحوم عبد الله العثمان الطبي في محافظة الأقصر في جمهورية مصر العربية, وتم على إثره إيداع الكتاب
+      ضمن مكتبة الأزهر الشريف، وللعديد من الشخصيات داخل وخارج الكويت.
+
+    </div>
+    <div class="col-lg-6 text-center">
+      <p>
+        ديوان العثمان
+      </p>
+      إصدار ديوان العثمان وهو: (الديوان الشعري للمرحوم عبد الله عبد اللطيف العثمان)، اعداد وتقديم م. عدنان عبدالله
+      العثمان.
+    </div>
+  </div>
+  <br><br><br>
+  <div class="row">
+    <div class="col-lg-6 text-center">
+      <img src="<?php echo e(asset('/uploads/book3.jpg')); ?>" width="300" height="500">
+
+    </div>
+
+    <div class="col-lg-6 text-center">
+      <img src="<?php echo e(asset('/uploads/book2.jpg')); ?>" width="300" height="500">
+
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="col-lg-6 text-center">
+      <p>
+        أفضل مقال
+      </p>
+      دعما لمسابقة المرحوم عبد الله عبد اللطيف العثمان لأفضل مقال، فقد تم طباعة كتاب يحوي أفضل 100 مقالة، وذلك لإعطاء
+      فرصة أكبر للانتشار للكتاب الشباب، وقد تم توزيع الكتاب مجانا على المهتمين بهذا المجال.
+    </div>
+    <div class="col-lg-6 text-center">
+      <p>
+        خطوات البدايات
+      </p>
+      كتيب خطوات البدايات ( الخطوبة الواضحة ) للدكتور راشد العليمي وبالتعاون مع مبرة المبرة الكويتية لحماية الأسرة
+      والاتحاد الوطني لطلبة الكويت، حيث يهدف الكتيب لتوعية المقبلين على الزواج بخطوات الخطوبة وما على الطرفين من حقوق
+      وواجبات.
+
+
+    </div>
+  </div>
+
+  <br><br><br>
+
+  <div class="row">
+
+    <div class="col-lg-6 text-center">
+      <img src="<?php echo e(asset('/uploads/book6.jpg')); ?>" width="300" height="500">
+
+    </div>
+
+    <div class="col-lg-6 text-center">
+      <img src="<?php echo e(asset('/uploads/book4.jpg')); ?>" width="300" height="500">
+
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-6 text-center">
+      <p>
+        كتاب للمكفوفين
+      </p>من الأفكار الجديدة التي قامت بها دار العثمان طباعة كتاب خاص بلغة برايل للمكفوفين، حيث تم توزيعه على هذه
+      الفئة.
+    </div>
+    <div class="col-lg-6 text-center">
+      <p>
+        الوحدة الوطنية
+      </p>
+      من الكتب القيمة التي رعتها "دار العثمان"، هو كتاب الوحدة الوطنية في كلمات سمو أمير دولة الكويت الشيخ صباح الأحمد
+      الجابر الصباح حفظه الله، من تأليف د. عصام عبد اللطيف الفليج، وهو كتاب يجمع كلمات صاحب السمو التي تحث على الوحدة
+      الوطنية، وتسطرها في كتيب مبسط وملون هادف، ليكون مرجعا هاما للطلبة ولجميع فئات المجتمع، وليكون لبنة مهمة لبناء
+      مجتمع مترابط ملتحم.
+    </div>
+  </div>
+
+
+  <br><br><br>
+
+  <div class="row">
+
+
+
+    <div class="col-lg-6 text-center">
+      <a href="/uploads/AL-OTHMANBOOK2.pdf"><img src="<?php echo e(asset('/uploads/book5.jpg')); ?>" width="300" height="500"></a>
+
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-6 text-center">
+      <p>
+        عثمان هذا الزمان
+      </p>
+      كتيب مبسط يحكي سيرة المرحوم عبد الله عبد اللطيف العثمان، وتم اعداده بالتعاون مع مؤسسة التواصل في اليمن.
+    </div>
+  </div>
+
 
 </div>
 
-<div class="col-lg-6 text-center">
-<img src="<?php echo e(asset('/uploads/version4.jpeg')); ?>" width="300" height="500">
-
-</div>
-</div>
-
-
-<div class="row">
-  <div class="col-lg-6 text-center">
-  <p>
-  ديوان العثمان
-</p>
-وهو الديوان الشعري للمرحوم عبدالله العثمان والذي يحتوي على لفيف من أشعاره الوطنية وأشعار الحكمة، من تقديم م. عدنان عبدالله العثمان.
-</div>
-<div class="col-lg-6 text-center">
-<p>
-أفضل مقال
-</p>
-دعما لمسابقة المرحوم عبد الله عبد اللطيف العثمان لأفضل مقال، فقد تم طباعة كتاب يحوي أفضل 100 مقالة بالمسابقة، وذلك لإعطاء فرصة أكبر للانتشار للكتاب الشباب.
-</div>
-</div>
-
- 
-            </div>
-
-            <?php $__env->stopSection(); ?>
+<?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
